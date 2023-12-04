acb130142ee780e68a536483ea7c913a1b1a4738
JrJrYoung <64763146+JrJrYoung@users.noreply.github.com>
2023-12-04 12:25:59 -0800

    first commit, boilerplates created

---
+++ main.py
@@ -0,0 +1,9 @@
+# Jonathan Young
+
+
+def main():
+    print("I should not be called")
+    
+if __name__ == "__main__":
+    main()
+    
---
+++ scraper.py
@@ -0,0 +1,8 @@
+# Jonathan Young
+
+
+def main():
+    print("I should not be called")
+    
+if __name__ == "__main__":
+    main()
